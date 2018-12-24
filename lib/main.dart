ed0f427088d1c9a11f188006eb94e8c800879d52
nicklin37 <nicklin369@gmail.com>
2018-12-23 22:19:17 -0800

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import './src/grocerylist.dart';
+import './src/inventory.dart';
+import './src/cook.dart';
 
 void main(){
   runApp(MyApp());
@@ -7,17 +10,27 @@ void main(){
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    return MaterialApp(
+      home: MyHome()
+    );
+  }
+}
 
-    Container buildPages(String name, String imagePath){
-      return Container(
-        margin: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-            width: 2.0,
-          ),
-          borderRadius: new BorderRadius.circular(20.0),
-        ),
+class MyHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+    RaisedButton buildPages(String name, String imagePath){
+      return RaisedButton(
+        padding: const EdgeInsets.all(10.0),
+        // decoration: BoxDecoration(
+        //   border: Border.all(
+        //     color: Colors.black,
+        //     width: 2.0,
+        //   ),
+        //   borderRadius: new BorderRadius.circular(20.0),
+        // ),
+
         child: Column(
           children: <Widget>[
             Container(
@@ -37,24 +50,39 @@ class MyApp extends StatelessWidget{
             ),
           ],
         ),
+        onPressed: () {
+          switch(name){
+            case "My Grocery List":
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GroceryList())
+              );
+              break;
+            case "Inventory":
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Inventory())
+              );
+              break;
+            case "Cook":
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Cook())
+              );
+              break;
+          }
+          
+        }
       );
     }
 
-    return MaterialApp(
-      title: 'Hungry Me',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Hungry Me'),
-        ),
-        body: ListView(
-          children: [
-            buildPages("Groceries", 'images/groceries.jpg'),
-            buildPages("Inventory", 'images/fridge.jpeg'),
-            buildPages("Cook", 'images/cooking.jpg'),
-          ],
-        ),
+    return Container(
+      child: ListView(
+        children: [
+          buildPages("My Grocery List", 'images/groceries.jpg'),
+          buildPages("Inventory", 'images/fridge.jpeg'),
+          buildPages("Cook", 'images/cooking.jpg'),
+        ],
       ),
     );
   }
